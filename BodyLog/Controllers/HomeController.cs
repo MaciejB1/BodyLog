bc38f14fbcbaead26ee185c9f2b1624325c3dc85
Maciej Babol <maciekbbol@gmail.com>
2019-04-18 18:25:26 +0200

    Big refactor, add CRUD, change dapper to entity framework

--- BodyLog/Controllers/HomeController.cs
+++ BodyLog/Controllers/HomeController.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using DataLibrary;
-using DataLibrary.BusinessLogic;
-
 
 namespace BodyLog.Controllers
 {
@@ -16,30 +13,7 @@ namespace BodyLog.Controllers
         {
             return View();
         }
-
-        public ActionResult AddProduct()
-        {
-            ViewBag.Message = "Add Product";
-
-            return View();
-
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult AddProduct(ProductModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                int RecordCreated = ProductsProcessor.AddProduct(model.Name, model.Calories, 
-                    model.Proteins, model.Carbohydrates, model.Fats);
-                return RedirectToAction("ViewProducts");
-            }
-
-            return View();
-            
-        }
-
+        
         public ActionResult FoodDiaryView()
         {
             ViewBag.Message = "Choose an option";
@@ -47,39 +21,6 @@ namespace BodyLog.Controllers
             return View();
         }
 
-        public ActionResult ViewProducts()
-        {
-            ViewBag.Message = "Products List";
-
-            var data = ProductsProcessor.LoadProducts();
-            List<ProductModel> products = new List<ProductModel>();
-
-            foreach (var row in data)
-            {
-                products.Add(new ProductModel
-                {
-                    Name = row.Name,
-                    Calories = row.Calories,
-                    Proteins = row.Proteins,
-                    Carbohydrates = row.Carbohydrates,
-                    Fats = row.Fats
-                });
-            }
-
-            return View(products);
-        }
-
-        public ActionResult EditProduct(ProductModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                int RecordCreated = ProductsProcessor.EditProduct(model.Name, model.Calories,
-                    model.Proteins, model.Carbohydrates, model.Fats, model.Id);
-                return RedirectToAction("ViewProducts");
-            }
-
-            return View();
-        }
 
     }
 }
